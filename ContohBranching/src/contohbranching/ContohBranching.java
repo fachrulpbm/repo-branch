c0cb9e0cf0f91ffd05eed4b1c13516301f3f21bb
beeboypralienka <fachrul.pbm@gmail.com>
2019-01-17 06:42:39 +0700

    modify method main

--- ContohBranching/src/contohbranching/ContohBranching.java
+++ ContohBranching/src/contohbranching/ContohBranching.java
@@ -4,6 +4,9 @@ public class ContohBranching {
 
     public static void main(String[] args) {
         
+        Person p  = new Mahasiswa("001", "Udin");
+        System.out.println(p.getNama());
+        
     }
     
 }
