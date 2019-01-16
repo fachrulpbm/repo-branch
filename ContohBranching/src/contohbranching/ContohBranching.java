49e912a5cb207955e85252a6b9eba60e556d01ab
fachrulpbm <fachrul.pbm@polindra.ac.id>
2019-01-17 06:46:10 +0700

    Modify main to dosen

--- ContohBranching/src/contohbranching/ContohBranching.java
+++ ContohBranching/src/contohbranching/ContohBranching.java
@@ -4,6 +4,9 @@ public class ContohBranching {
 
     public static void main(String[] args) {
         
+        Person p = new Dosen("123", "Joko");
+        System.out.println(p.getNama());
+        
     }
     
 }
